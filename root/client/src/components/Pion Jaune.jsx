a55f41611d6c11047bb170cae0fc3b56c54f779c
Diez <62673882+zeidfazazi@users.noreply.github.com>
2021-10-26 22:10:54 +0200

    Add code to implement the pawn movement (no shock)

--- root/client/src/components/Pion Jaune.jsx
+++ root/client/src/components/Pion Jaune.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as Yellow } from '../assets/Pion Jaune.svg'
 import '../style/Pion.css'
 
-const PionJaune = () => {
+const PionJaune = ({ x, y, power, handlePlay }) => {
+  const [posX, setPosX] = useState(x)
+  const handleMovement = () => {
+    setPosX(handlePlay(posX, y, power))
+  }
   return (
     <>
-      <Yellow id='yellowpiece' />
+      <Yellow id='yellowpiece' onClick={() => handleMovement()} />
     </>
   )
 }
--- root/client/src/components/Pion Rouge.jsx
+++ root/client/src/components/Pion Rouge.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as Red } from '../assets/Pion Rouge.svg'
+
 import '../style/Pion.css'
-const PionRouge = (order) => {
-  const style = { '--order': order }
+const PionRouge = ({ x, y, power, handlePlay }) => {
+  const [posY, setPosY] = useState(y)
+  const handleMovement = () => {
+    setPosY(handlePlay(x, posY, power))
+  }
   return (
     <>
-      <Red id='redpiece' style={style} />
+      <Red id='redpiece' onClick={() => handleMovement()} />
     </>
   )
 }
