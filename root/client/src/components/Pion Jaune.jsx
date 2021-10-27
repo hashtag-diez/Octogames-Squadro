d22e8e9ecc5462548f17931f62cd714fb84b15c8
Diez <62673882+zeidfazazi@users.noreply.github.com>
2021-10-27 22:30:46 +0200

    Add code to implement back-and-forth movement and score incrementation

--- root/client/src/components/Pion Jaune.jsx
+++ root/client/src/components/Pion Jaune.jsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 import { ReactComponent as Yellow } from '../assets/Pion Jaune.svg'
 import '../style/Pion.css'
 
-const PionJaune = ({ x, y, power, handlePlay }) => {
+const PionJaune = ({ x, y, powerGo, powerReturn, handlePlay }) => {
   const [posX, setPosX] = useState(x)
-  const handleMovement = () => {
-    setPosX(handlePlay(posX, y, power))
+  const [currPower, setCurrPower] = useState(powerGo)
+  const handleMovement = (e) => {
+    e.preventDefault()
+    if (currPower !== 0) {
+      const newPosX = handlePlay(posX, y, currPower)
+      if (newPosX === 0) setCurrPower(0)
+      if (newPosX === 6) setCurrPower(powerReturn)
+      setPosX(newPosX)
+    } else {
+      console.log('Pion inactif !')
+    }
   }
   return (
     <>
-      <Yellow id='yellowpiece' onClick={() => handleMovement()} />
+      <Yellow id='yellowpiece' onClick={(e) => handleMovement(e)} />
     </>
   )
 }
