60a3995013303cf396cbbd67a59e74e05710f2f6
Diez <62673882+zeidfazazi@users.noreply.github.com>
2021-10-31 16:23:59 +0100

    Add code for simple onclick animation

--- root/client/src/components/Pion Rouge.jsx
+++ root/client/src/components/Pion Rouge.jsx
@@ -1,8 +1,10 @@
+import styled, { keyframes } from 'styled-components'
 import React, { useState } from 'react'
 import { ReactComponent as Red } from '../assets/Pion Rouge.svg'
 
 import '../style/Pion.css'
 const PionRouge = ({ x, y, powerGo, powerReturn, handlePlay }) => {
+  const [animate, setAnimate] = useState('')
   const [posY, setPosY] = useState(y)
   const [currPower, setCurrPower] = useState(powerGo)
   const handleMovement = (e) => {
@@ -15,15 +17,29 @@ const PionRouge = ({ x, y, powerGo, powerReturn, handlePlay }) => {
         setCurrPower(powerReturn)
       }
       setPosY(newPosY)
+      setAnimate('true')
     } else {
       console.log('%cPion inactif...', 'font-style: italic')
     }
   }
   return (
     <>
-      <Red id='redpiece' onClick={(e) => handleMovement(e)} />
+      <StyledRed animate={animate} curr={posY} step={currPower} onClick={(e) => handleMovement(e)} />
     </>
+
   )
 }
+const slide = (a, b) =>
+  keyframes`
+    0%{
+      transform: translateX(${a}px);
+    }
+    100%{
+      transform: translateX(${b}px);
+    }
+`
+const StyledRed = styled(Red)`
+  animation: ${({ animate, curr, step }) => (animate === 'true' ? slide(curr * 94 - step * 94, curr * 94) : '')} 0.3s ease-in-out forwards;
+`
 
 export default PionRouge
