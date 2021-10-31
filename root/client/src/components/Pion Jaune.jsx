bfc5e636ab466dec47541d565124f65769fc7e85
Diez <62673882+zeidfazazi@users.noreply.github.com>
2021-10-31 16:23:59 +0100

    Add code for simple onclick animation

--- root/client/src/components/Pion Jaune.jsx
+++ root/client/src/components/Pion Jaune.jsx
@@ -1,11 +1,14 @@
+import styled, { keyframes } from 'styled-components'
 import React, { useState } from 'react'
 import { ReactComponent as Yellow } from '../assets/Pion Jaune.svg'
 import '../style/Pion.css'
 
 const PionJaune = ({ x, y, powerGo, powerReturn, handlePlay }) => {
   const [posX, setPosX] = useState(x)
+  const [animate, setAnimate] = useState('')
   const [currPower, setCurrPower] = useState(powerGo)
   const handleMovement = (e) => {
+    setAnimate('false')
     e.preventDefault()
     if (currPower !== 0) {
       const newPosX = handlePlay(posX, y, currPower)
@@ -18,12 +21,44 @@ const PionJaune = ({ x, y, powerGo, powerReturn, handlePlay }) => {
     } else {
       console.log('%cPion inactif...', 'font-style: italic')
     }
+    setAnimate('true')
   }
   return (
-    <>
-      <Yellow id='yellowpiece' onClick={(e) => handleMovement(e)} />
-    </>
+    <StyledDiv animate={animate} curr={posX} step={currPower} onClick={(e) => handleMovement(e)}>
+      <Yellow />
+    </StyledDiv>
   )
 }
-
+const StaticSlide = keyframes`
+  0%{
+    transform: translateY(0px);
+  }
+  100%{
+    transform: translateY(92px);
+  }
+`
+const slide = (a, b) =>
+  keyframes`
+    0%{
+      transform: translateY(${a}px);
+    }
+    100%{
+      transform: translateY(${b}px);
+    }
+`
+/* const slideWithRotate = (a, b) => {
+  keyframes`
+  0% {
+    transform: translateY(${a}px);
+  }
+  75%{
+    transform: translateY(${b}px) rotate(0deg);
+  }
+  100%{
+    transform: translateY(${b}px) rotate(180deg);
+  }`
+} */
+const StyledDiv = styled.div`
+  animation: ${({ animate, curr, step }) => (animate === 'true' ? slide(curr * 94 - step * 94, curr * 94) : '')} 0.3s ease-in-out forwards;
+`
 export default PionJaune
