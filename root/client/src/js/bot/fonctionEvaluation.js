ac0bebeca81dcde3442c9dff76a394982cee2a83
Sarahhabbi <habbisarah.1@gmail.com>
2022-01-12 13:31:12 +0100

    tested evaluation, min max and next move functions, fixed some bugs

--- root/client/src/js/bot/fonctionEvaluation.js
+++ root/client/src/js/bot/fonctionEvaluation.js
@@ -2,27 +2,28 @@ import PARAMETRES from '../../json/evaluation.json'
 
 //retourne la valeur d'un coup
 export default function Evaluation(idPion, listYellow, listRed, currPlateau, currPower){
-    const pawn = listYellow.filter(pion =>  pion.id === idPion)
-    console.log("le pouvoir :")
-    console.log(currPower)
-    const pawnParameters = { 
-        id: idPion, 
-        AR: (currPower > 0)? 1: 0,
+    const pawn = listYellow.filter(pion =>  pion.id === idPion);
+    const x = pawn[0].x;
+    const y = pawn[0].y;
+    const aller = pawn[0].aller;
+    console.log("le pouvoir : ", currPower);
+    const pawnParameters = {
+        id: idPion,
+        AR: (currPower > 0)? 1 : 0,
         move : currPower,
-        risk : evalRisk(pawn[0].x, pawn[0].y, currPlateau, listRed),
-        menace : evalMenace(pawn[0].x, pawn[0].y, currPlateau),
-        staking : evalStack(pawn[0].x, listYellow),
-        distance : (currPower > 0)? (6 - pawn[0].x)/6 : (6 + pawn[0].x)/6
+        risk : evalRisk(x, y, currPlateau, listRed),
+        menace : evalMenace(x, y, "yellow", currPlateau),
+        staking : evalStack(x, listYellow),
+        distance : (currPower > 0)? (6 - currPower)/6 : (6 + currPower)/6
     }
-    console.log(pawnParameters)
-    const allerRetour = (pawnParameters.AR === 1) ? PARAMETRES['A/R'].ALLER : PARAMETRES['A/R'].RETOUR
+    //console.log(pawnParameters)
+    const allerRetour = (aller === 1) ? PARAMETRES['A/R']['RETOUR'] : PARAMETRES['A/R']['ALLER'];
     const deplacement = PARAMETRES['DEPLACEMENT'][`${pawnParameters.move}`]
-    const risque = pawnParameters.distance * pawnParameters.risk * PARAMETRES.RISQUE 
+    const risque = pawnParameters.distance * pawnParameters.risk * PARAMETRES.RISQUE
     const menace = pawnParameters.menace * PARAMETRES.MENACE
     const staking = pawnParameters.staking * PARAMETRES.STAKING
-    return allerRetour + deplacement + risque + menace + staking
+    return allerRetour + deplacement + risque + menace + staking;
 }
-
 function evalRisk(x, y, currPlateau, listRed){
     /*console.log(currPlateau)
     console.log(x + " : " +y)
