3b312631b051a21e405c63775846c732e675d7d2
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-01-13 12:14:52 +0100

    Add code to handle differents pages redirections

--- root/client/src/pages/Home.jsx
+++ root/client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/Logo.png'
 import { ReactComponent as Board } from '../assets/Plateau.svg'
 import { ReactComponent as Rules } from '../assets/Regles.svg'
@@ -8,52 +8,79 @@ import { ReactComponent as Return } from '../assets/Retour.svg'
 import { ReactComponent as Danger } from '../assets/Danger.svg'
 import { ReactComponent as Bot } from '../assets/bot.svg'
 import { ReactComponent as Duel } from '../assets/duel.svg'
+import { ReactComponent as Local } from '../assets/local.svg'
+import { ReactComponent as Network } from '../assets/wifi.svg'
+import { ReactComponent as Back } from '../assets/back arrow.svg'
 
 import styled from 'styled-components'
+
 const Home = () => {
+  const [multi, setMulti] = useState()
+  const [local, setLocal] = useState()
+
+  /* if (!multi) {
+    // return(Route page BotGame)
+  } else if (multi && local) {
+    // return(Route page LocalGame)
+  } else if (multi && !local) {
+    // return(Route page NetworkGame)
+  } else { */
   return (
-    <>
-      <StyledHeader>
-        <img src={logo} alt='logo' />
-      </StyledHeader>
-      <StyledMain>
-        <Board />
-        <aside>
-          <h1>Le <StyledSpan yellow>meilleur</StyledSpan> <br /> endroit pour jouer. <br /> Tout <StyledSpan yellow={false}>simplement</StyledSpan>.</h1>
-          <List>
-            <HeaderList>
-              <Rules />
-              <h2>Règles</h2>
-            </HeaderList>
-            <ElementList>
-              <Medal />
-              <p>Le premier à faire un aller-retour avec 4 de ses 5 pièces sur le plateau a gagné</p>
-            </ElementList>
-            <ElementList>
-              <Arrow />
-              <p>Les pions avancent selon la force indiquée sur leur zone de départ</p>
-            </ElementList>
-            <ElementList>
-              <Return />
-              <p>Une fois de l’autre côté, les pions se retournent et avancent selon la force indiquée sur cette zone</p>
-            </ElementList>
-            <ElementList>
-              <Danger />
-              <p>Si un pion est traversé par un pion adverse,  il retourne dans sa base</p>
-            </ElementList>
-          </List>
-          <Button color='#E02016'> <Bot /> Jouer contre <br /> l'ordinateur </Button>
-          <Button color='#DAA25D'> <Duel /> Jouer contre <br /> un adversaire </Button>
-        </aside>
-      </StyledMain>
-      <StyledFooter>
-        <p>
-          Sorbonne Université © - 2021
-        </p>
-      </StyledFooter>
-    </>
-  )
-}
+      <>
+        <StyledHeader>
+          <img src={logo} alt='logo' />
+        </StyledHeader>
+        <StyledMain>
+          <Board />
+          <aside>
+            <h1>Le <StyledSpan yellow>meilleur</StyledSpan> <br /> endroit pour jouer. <br /> Tout <StyledSpan yellow={false}>simplement</StyledSpan>.</h1>
+            <List>
+              <HeaderList>
+                <Rules />
+                <h2>Règles</h2>
+              </HeaderList>
+              <ElementList>
+                <Medal />
+                <p>Le premier à faire un aller-retour avec 4 de ses 5 pièces sur le plateau a gagné</p>
+              </ElementList>
+              <ElementList>
+                <Arrow />
+                <p>Les pions avancent selon la force indiquée sur leur zone de départ</p>
+              </ElementList>
+              <ElementList>
+                <Return />
+                <p>Une fois de l’autre côté, les pions se retournent et avancent selon la force indiquée sur cette zone</p>
+              </ElementList>
+              <ElementList>
+                <Danger />
+                <p>Si un pion est traversé par un pion adverse,  il retourne dans sa base</p>
+              </ElementList>
+            </List>
+            <ButtonsWrapper>
+              {!multi ?
+                <>
+                  <Button color='#34495e' onClick={() => setMulti(false)}> <Bot /> Jouer contre <br /> l'ordinateur </Button>
+                  <Button color='#da8012' onClick={() => setMulti(true)}> <Duel /> Jouer contre <br /> un adversaire </Button>
+                </>
+                :
+                <>
+                  <Button color='#E02016' onClick={() => setLocal(false)}> <Local /> Jeu en local </Button>
+                  <Button color='#DAA25D' onClick={() => setLocal(false)}> <Network /> Jeu en réseau </Button>
+                  <BackButton color='#34495e9e' onClick={() => setMulti()}><Back /> </BackButton>
+                </>
+              }
+            </ButtonsWrapper>
+          </aside>
+        </StyledMain>
+        <StyledFooter>
+          <p>
+            Sorbonne Université © - 2021
+          </p>
+        </StyledFooter>
+      </>
+    )
+  }
+// }
 const StyledHeader = styled.header`
   height: 11%;
   padding-top : 20px;
@@ -72,7 +99,8 @@ const Button = styled.button`
   margin-top: 10px;
   font-size: 18px;
   font-weight: 700;
-  width: auto;
+  width: 260px;
+  height: 73px;
   line-height: 108.5%;
   letter-spacing: 2px;
   margin: 25px 0px;
@@ -83,7 +111,29 @@ const Button = styled.button`
   gap: 15px;
   svg{
     width: 18% !important;
+    min-height: 38px;
   }
+`
+const ButtonsWrapper = styled.div`
+  position: relative;
+  overflow: visible;
+`
+const BackButton = styled(Button)`
+  position: absolute;
+  bottom: -50px;
+  margin-left: auto !important;
+  margin-right: auto !important;
+  left: 0;
+  right: 0;
+  svg{
+    width: 10px !important;
+    height: 20px !important;
+  }
+  width: auto;
+  padding: 3px 15px;
+  opacity: 0.7;
+  margin: 0px 0px;
+  height: auto;
 `
 const StyledFooter = styled.footer`
   display: flex;
@@ -151,6 +201,8 @@ const StyledMain = styled.main`
     width: 40%;
     align-items: center;
     justify-content: center;
+    overflow: visible;
+
     h1 {
       margin: 0;
       font-size: 42px;
